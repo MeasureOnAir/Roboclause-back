a80bf4c5ad11c7acf97281b9683f457f8de49dc3
SahanAtNeura <sahan@neuraintelligence.com>
2023-07-31 14:19:27 +0200

    Added Main Function to router to 0.0.0.0

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def root():
     return {"message": "Welcome to Roboclause!", "status": "🟢 Alive"}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
